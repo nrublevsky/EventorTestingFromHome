3883be71c8a17704f828c44ec73b48270523b7b2
nrublevsky <nickrublevsky@gmail.com>
2018-12-16 17:58:57 +0200

    Trying to run shit

--- app/src/main/java/Base.java
+++ app/src/main/java/Base.java
@@ -12,7 +12,7 @@ public class Base {
     private static AndroidDriver driver;
 
     @Test
-    public void TCase() throws MalformedURLException {
+    public void TCase() throws MalformedURLException, InterruptedException, IllegalMonitorStateException {
 
 
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -20,17 +20,20 @@ public class Base {
         caps.setCapability("udid", "emulator-5554");
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "8.0");
-        caps.setCapability("appPackage", "com.rozdoum.eventor.couchbase");
-        caps.setCapability("appActivity", "com.rozdoum.eventor.activities.MainActivity");
- //       caps.setCapability("noReset", "true");
+        caps.setCapability("appPackage", "com.rozdoum.eventor.lpmd2018");
+        caps.setCapability("appActivity", "com.rozdoum.eventor.activities.SchedulerActivity");
+//        caps.setCapability("noReset", "true");
 
 
         URL url = new URL("http://0.0.0.0:4723/wd/hub");
         AndroidDriver driver = new AndroidDriver(url, caps);
 
         driver.launchApp();
-        driver.findElement(By.id("com.rozdoum.eventor.couchbase:id/action_search")).click();
-        driver.findElement(By.className("android.support.v7.app.ActionBar$Tab")).click();
+//        driver.wait(60000);
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout")).isDisplayed();
+        driver.findElement(By.xpath("//android.widget.ImageButton[@content-desc=\"Open navigation drawer\"]")).click();
+//        driver.findElement(By.id("com.rozdoum.eventor.lpmd2018:id/drawableImageView")).click();
+//        driver.findElement(By.className("android.support.v7.app.ActionBar$Tab")).click();
     }
 
 
